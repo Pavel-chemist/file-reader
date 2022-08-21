13e19f77cfd6bcf5fabd7db0685131759a95a803
Pavel-chemist <pavel.chemist@gmail.com>
2022-08-21 13:45:51 +0400

    implement file load

--- main.js
+++ main.js
@@ -1,11 +1,39 @@
 // Modules to control application life and create native browser window
-const { app, BrowserWindow } = require('electron')
-const path = require('path')
+const { app, BrowserWindow, ipcMain, dialog } = require('electron');
+const path = require('path');
+const fs = require('fs'); // Load the File System to execute our common tasks (CRUD)
+
+async function handleFileOpen() {
+  const { canceled, filePaths } = await dialog.showOpenDialog()
+  if (canceled) {
+    console.log("No file selected");
+
+    return;
+  } else {
+    const data = ReadFile(filePaths[0]);
+    
+    return [filePaths[0], data];  
+  }
+}
+
+function ReadFile(filePath) {
+  console.log('path to file to read: ', filePath);
+
+  const readData = fs.readFileSync(filePath, 'utf-8', (err, data) => {
+    if(err){
+        alert("An error ocurred reading the file :" + err.message);
+        return;
+    }
+    return data;
+  });
+
+  return readData;
+}
 
 function createWindow () {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1200,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js')
@@ -16,27 +44,17 @@ function createWindow () {
   mainWindow.loadFile('index.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
 app.whenReady().then(() => {
-  createWindow()
+  ipcMain.handle('dialog:openFile', handleFileOpen);
+  // ipcMain.handle('fs:readFile', handleReadFile);
 
-  app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
-
-// Quit when all windows are closed, except on macOS. There, it's common
-// for applications and their menu bar to stay active until the user quits
-// explicitly with Cmd + Q.
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
+  createWindow();
 })
 
 // In this file you can include the rest of your app's specific main process
--- renderer.js
+++ renderer.js
@@ -4,3 +4,18 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const btn = document.getElementById('btn');
+const filePathElement = document.getElementById('filePath');
+const fileContentElement = document.getElementById('fileContent');
+
+btn.addEventListener('click', async () => {
+  const filePathAndData = await window.electronAPI.openFile();
+  
+  console.log('filepath to use for file opening: ', filePathAndData[0]);
+  console.log('file contents: ', filePathAndData[1]);
+  
+  filePathElement.innerText = filePathAndData[0];
+  fileContentElement.innerText = filePathAndData[1];
+  
+});
