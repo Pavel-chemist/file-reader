4e69b8126a7b93ee9613e5f5ef2592713de0abbe
Pavel-chemist <pavel.chemist@gmail.com>
2022-08-22 11:05:18 +0400

    add exception for cr2 files

--- src/renderer.ts
+++ src/renderer.ts
@@ -12,17 +12,30 @@ let view: string = 'ascii';
 const filePathElement = document.getElementById('filePath');
 const fileContentElement = document.getElementById('fileContent');
 
+const imgeViewElement = document.getElementById('imageView');
+
 let fileHexString: string = '';
 
 btn!.addEventListener('click', async () => {
   console.log('button is clicked');
 
-  const filePathAndData = await window.electronAPI.openFile();
+  const filePathAndData: string[] = await window.electronAPI.openFile();
+  const fileExtension: string = filePathAndData[0].split('.').pop().toLowerCase();
   
   filePathElement!.innerText = filePathAndData[0];
 
+  console.log('file extension:', fileExtension);
+
   fileHexString = filePathAndData[1];
-  convertFileHexString();
+
+  if (fileExtension !== 'cr2') {
+    convertFileHexString();
+    slct!.classList.remove('hide');
+  } else {
+    fileContentElement!.innerText = 'cr2 files are to be presented as image';
+    slct!.classList.add('hide');
+  }
+  
 });
 
 slct!.addEventListener('input', () => {
@@ -113,3 +126,87 @@ function formIndexSubstr(i: number): string {
     return `${lineNumber} | `;
   }
 }
+
+
+
+/* 
+WARR ArrangeBytesToWords ( CARR charArray )
+{
+//	cout << "\n---1---\n";
+	WARR wordArray;	
+	bool reflectRows = true;
+//	cout << "\n---2---\n";
+	int size = charArray.size;	
+
+//	cout << "\n---3---\n";
+	WORD *p_wordArray = new WORD [ size * 8 / 12 ];
+	
+//	cout << "\n---4---\n";
+	WORD b0, b1, b2, b3, b4, b5; 
+	WORD w0 = 0, w1 = 0, w2 = 0, w3 = 0;			//12 bits go into each word
+	WORD mask = 0x0fff; 
+	
+	
+	int index = 0;
+//maybe reflect rows? - do this with switch	
+	int rowIndex = 0;
+//  4 pixels at a time, IMAGE_WIDTH/4 (930) times per row
+//	starting at the end of row, moving backwards to zeroeth pixel, then starting at the end of next row
+//	cout << "\n---5---\n";
+	for ( int i = 0; i < size; i+=6 )
+	{
+		b0 = charArray.p_Array[i + 0];
+		b1 = charArray.p_Array[i + 1];
+		b2 = charArray.p_Array[i + 2];
+		b3 = charArray.p_Array[i + 3];
+		b4 = charArray.p_Array[i + 4];
+		b5 = charArray.p_Array[i + 5];
+	/**
+	*	Bit packing of sensor data
+	*
+	*	byte|0       |1       |2       |3       |4       |5       |
+	*	pix	|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
+	*	0___|3210____|BA987654|________|________|________|________|
+	*	1___|____BA98|________|________|76543210|________|________|
+	*	2___|________|________|BA987654|________|________|3210____|
+	*	3___|________|________|________|________|76543210|____BA98|
+	**/
+  /*
+  w0 = ((b1 << 4) + (b0 >> 4)) & mask;
+  w1 = ((b0 << 8) + (b3)) & mask;
+  w2 = ((b2 << 4) + (b5 >> 4)) & mask;
+  w3 = ((b5 << 8) + (b4)) & mask;
+  
+  index = i * 2 / 3;
+  rowIndex = (index / IMAGE_WIDTH) + 1; //start with row 1
+  
+  if ( reflectRows )
+  {	
+    p_wordArray[rowIndex*IMAGE_WIDTH - index%IMAGE_WIDTH - 1] = w0;
+    p_wordArray[rowIndex*IMAGE_WIDTH - index%IMAGE_WIDTH - 2] = w1;
+    p_wordArray[rowIndex*IMAGE_WIDTH - index%IMAGE_WIDTH - 3] = w2;
+    p_wordArray[rowIndex*IMAGE_WIDTH - index%IMAGE_WIDTH - 4] = w3;
+    
+
+  }
+  else
+  {
+    p_wordArray[index + 0] = w0;
+    p_wordArray[index + 1] = w1;
+    p_wordArray[index + 2] = w2;
+    p_wordArray[index + 3] = w3;
+  }
+}
+//	cout << "\n---6---\n";
+wordArray.isReflected = reflectRows;
+wordArray.size = size * 8 / 12;
+wordArray.width = IMAGE_WIDTH;
+wordArray.height = IMAGE_HEIGHT;
+wordArray.name = charArray.name;
+wordArray.p_Array = p_wordArray;	
+
+//	cout << "\n---7---\n";
+return wordArray;
+}
+
+*/
